8a8f7634a5c500a57fbe0cd3414c58f7e04899e5
Emil Åkerman <emil.akerman@iths.se>
2023-03-16 16:11:37 +0100

    added list of currency pairs that updates daily-ish

--- apifile.js
+++ apifile.js
@@ -1,9 +1,19 @@
 
 const host = 'api.frankfurter.app';
+let oneWeekAgo = getWeekAgoDate();
+let urlOneWeekAgo = `https://api.frankfurter.app/${oneWeekAgo}?from=USD`;
+let lastWeekValue = '';
+let currencyList = document.getElementById('currencyList');
+
+
+
+
+
+// https://api.frankfurter.app/latest?from=USD
+let latestFromUSD = 'https://api.frankfurter.app/latest?from=USD';
 
 let selectedReturnCurrency = '';
 let amountInputValue = 0;
-
 let amountInput = document.getElementById('amountInput');
 const convertButton = document.getElementById('convertButton');
 const clearButton = document.getElementById('clearButton');
@@ -52,3 +62,76 @@ const fetchandDisplay = (selectedReturnCurrency, amountInputValue) => {
         }
       });
 }
+///
+window.onload = function() {
+    // createCurrencyElements();
+    populateList();
+  };
+
+const createCurrencyElements = () => {
+    fetch(`https://${host}/latest?amount=1&from=USD&to=GBP`)
+        .then(resp => resp.json())
+        .then((data) => { 
+            const currencyElement = document.createElement('h3');
+            currencyElement.className = 'currencyElement';
+            currencyElement.innerHTML = (`USD-GBP ${data.rates.GBP}`);
+        
+            const currencyList = document.getElementById('currencyList');
+        
+            currencyList.appendChild(currencyElement);
+        });
+
+    return currencyElement;
+} 
+const populateList = () => {
+
+
+    fetch(latestFromUSD)
+    .then(resp => resp.json())
+    .then((data) => { 
+            for (const key in data.rates) {
+                let currencyElement = document.createElement('h4');
+                let valueElement = document.createElement('h4');
+                let dateElement = document.createElement('h4');
+
+                currencyElement.className = 'currencyElement';
+                valueElement.className = 'valueElement';
+
+                currencyElement.innerHTML = (`USD-${key}`);
+                valueElement.innerHTML = (`${data.rates[key]}`);
+                dateElement.innerHTML = (data.date);
+
+                currencyList.appendChild(currencyElement);
+                currencyList.appendChild(valueElement);
+                currencyList.appendChild(dateElement); 
+                //currencyList.appendChild(returnLastWeekValue()); 
+                //currencyList.appendChild.innerHTML(returnLastWeekValue());             
+            }
+    });
+
+}
+function getWeekAgoDate() {
+    const t = new Date();
+    const date = ('0' + (t.getDate() - 7 )).slice(-2);
+    const month = ('0' + (t.getMonth() + 1)).slice(-2);
+    const year = t.getFullYear();
+    let oneWeekAgo = `${year}-${month}-${date}`;
+    return oneWeekAgo;
+  }
+
+  const returnLastWeekValue = () => {
+    fetch(urlOneWeekAgo)
+    .then(resp => resp.json())
+    .then((data) => {
+    for (const key in data.rates) {
+            let weekChange = document.createElement('h4');
+            weekChange.innerHTML = (`${data.rates[key]}`);
+            currencyList.appendChild(weekChange);
+            return weekChange;
+
+            //lastWeekValue = data.rates[key];
+            //console.log(lastWeekValue);
+            //return lastWeekValue;
+        }
+    });
+  }
