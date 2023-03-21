d949d3b5f23f6dd524a07a3cdd7cc7705b45b1db
Emil Åkerman <emil.akerman@iths.se>
2023-03-21 14:21:40 +0100

    some more struture fixes and removal of unneeded section

--- apifile.js
+++ apifile.js
@@ -4,64 +4,9 @@ let oneWeekAgo = getWeekAgoDate();
 let urlOneWeekAgo = `https://api.frankfurter.app/${oneWeekAgo}?from=USD`;
 let lastWeekValue = '';
 let currencyList = document.getElementById('currencyList');
-
-
-
-
-
-// https://api.frankfurter.app/latest?from=USD
 let latestFromUSD = 'https://api.frankfurter.app/latest?from=USD';
-
 let selectedReturnCurrency = '';
-let amountInputValue = 0;
-let amountInput = document.getElementById('amountInput');
-const convertButton = document.getElementById('convertButton');
-const clearButton = document.getElementById('clearButton');
-const selectedSEK = document.getElementById('SEK');
-const selectedGBP = document.getElementById('GBP');
-const selectedNOK = document.getElementById('NOK');
 
-convertButton.addEventListener('click', () => {
-    if (amountInput.value != '') {
-        if (selectedSEK.checked) {
-            selectedReturnCurrency = 'SEK';
-        }
-        if (selectedGBP.checked) {
-            selectedReturnCurrency = 'GBP';
-        }
-        if (selectedNOK.checked) {
-            selectedReturnCurrency = 'NOK';
-        }
-        fetchandDisplay(selectedReturnCurrency, amountInputValue = amountInput.value);
-    } else {
-        console.log('the input field is empty');
-    }
-});
-clearButton.addEventListener('click', () => {
-    clearInput();
-});
-
-function clearInput() {
-    if (amountInput.value != '') {
-        amountInput.value = '';
-        document.querySelector('input[name="radioButtons"]:checked').checked = false;
-    }
-}
-const fetchandDisplay = (selectedReturnCurrency, amountInputValue) => {
-    fetch(`https://${host}/latest?amount=${amountInputValue}&from=USD&to=${selectedReturnCurrency}`)
-      .then(resp => resp.json())
-      .then((data) => {
-        if (selectedReturnCurrency == 'SEK') {
-            alert(`${amountInputValue} USD = ${data.rates.SEK} ${selectedReturnCurrency}`);
-        }
-        if (selectedReturnCurrency == 'GBP') {
-            alert(`${amountInputValue} USD = ${data.rates.GBP} ${selectedReturnCurrency}`);
-        }
-        if (selectedReturnCurrency == 'NOK') {
-            alert(`${amountInputValue} USD = ${data.rates.NOK} ${selectedReturnCurrency}`);
-        }
-      });
-}
 ///
 window.onload = function() {
     // createCurrencyElements();
@@ -144,6 +89,14 @@ const returnAUD = document.getElementById('option5');
 const returnEUR = document.getElementById('option6');
 const returnINR = document.getElementById('option7');
 const returnHKD = document.getElementById('option8');
+const fromSEK = document.getElementById('option1from');
+const fromGBP = document.getElementById('option2from');
+const fromNOK = document.getElementById('option3from');
+const fromJPY = document.getElementById('option4from');
+const fromAUD = document.getElementById('option5from');
+const fromEUR = document.getElementById('option6from');
+const fromINR = document.getElementById('option7from');
+const fromHKD = document.getElementById('option8from');
 
 left_input.addEventListener("input", () => {
     if (left_input.value != '') {
@@ -176,9 +129,9 @@ left_input.addEventListener("input", () => {
         right_output.value = '';
     }
 });
-
+let fromCurrency = 'USD';
 const fetchandDisplay2 = (inputValue, selectedReturnCurrency) => {
-    fetch(`https://${host}/latest?amount=${inputValue}&from=USD&to=${selectedReturnCurrency}`)
+    fetch(`https://${host}/latest?amount=${inputValue}&from=${fromCurrency}&to=${selectedReturnCurrency}`)
       .then(resp => resp.json())
       .then((data) => {
         switch (selectedReturnCurrency) {
